d5ada3655525759a05978242c2414aa3d15138b9
Sap79 <131272085+Sap79@users.noreply.github.com>
2023-04-19 19:36:24 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+def user_input_numbers():
+  first_number = float(st.number_input('First number'))
+  second_number = float(st.number_input('Second number'))
+  third_number = float(st.number_input('Third number'))
+  numbers_list = [first_number,second_number,third_number]
+  return numbers_list
+st.header('User Input Numbers')
+L = user_input_numbers()
+max = np.array(L).max()
+st.subheader('The maximum number')
+st.write('The maximum number is:'+str(max))
